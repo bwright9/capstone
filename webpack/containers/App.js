7992f4e1d914b239fdeb3015e9de4e27a6126441
Tom Lynam <tom.s.lynam@gmail.com>
2016-06-30 15:44:48 -0600

    side nav and top nav

--- webpack/containers/App.js
+++ webpack/containers/App.js
@@ -1,8 +1,36 @@
 import React from 'react';
 
 const App = () => (
+	
   <div>
-    Hello World
+
+		<nav>
+	    <div className="nav-wrapper">
+	      <a href="#" className="brand-logo">somethere</a>
+	      <ul id="nav-mobile" className="right hide-on-med-and-down">
+	        <li><a href="#!" className="btn">Login</a></li>
+	      </ul>
+	    </div>
+	  </nav>
+
+		
+    <ul id="slide-out" className="side-nav fixed">
+      <li className="search">
+      	<div className="search-wrapper  focused">
+      		<input id="search" placeholder="Search" />
+      	</div>
+      </li>
+      <li><a href="#!">Move</a></li>
+      <li><a href="#!">Visit</a></li>
+      <li><a href="#!">Discover</a></li>
+      <li><a href="#!">Favorites</a></li>
+      <li><p className="" id="city">Current City: </p></li>
+
+    </ul>
+    <a href="#" data-activates="slide-out" className="button-collapse"><i className="mdi-navigation-menu"></i></a>
+
+    <h1 className="center">Hello World</h1>
+
   </div>
 )
 
